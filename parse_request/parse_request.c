22e6dd330e1e083816d3de2268a58ca7ff817eca
test <test@test.fr>
2020-09-26 12:19:07 +0200

    [QUERIES] Parse queries

---
+++ parse_request/parse_request.c
@@ -0,0 +1,104 @@
+//
+// Created by denis on 26/09/2020.
+//
+
+#include "parse_request.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <netinet/in.h>
+
+void print_header(dns_header *dns_header)
+{
+    printf("%d\n", dns_header->id);
+    printf("%d\n", dns_header->qr);
+    printf("%d\n", dns_header->opcode);
+    printf("%d\n", dns_header->aa);
+    printf("%d\n", dns_header->tc);
+    printf("%d\n", dns_header->rd);
+    printf("%d\n", dns_header->ra);
+    printf("%d\n", dns_header->z);
+    printf("%d\n", dns_header->rcode);
+    printf("%d\n", dns_header->qdcount);
+    printf("%d\n", dns_header->ancount);
+    printf("%d\n", dns_header->nscount);
+    printf("%d\n", dns_header->arcount);
+}
+void print_question(dns_question *dns_question)
+{
+    printf("%s \n", dns_question->qname);
+    printf("%d \n", dns_question->qtype);
+    printf("%d \n", dns_question->qclass);
+}
+
+void print_response(dns_response *dns_response)
+{
+    printf("%s \n", dns_response->name);
+    printf("%d \n", dns_response->type);
+    printf("%d \n", dns_response->class);
+    printf("%d \n", dns_response->ttl);
+    printf("%d \n", dns_response->rdlength);
+    printf("%d \n", dns_response->rdata);
+}
+
+dns_header *init_dns_header()
+{
+    dns_header *header = calloc(1, sizeof(dns_header));
+    if (header == NULL)
+        return NULL;
+    header->opcode = OPCODE_VALUE;
+    header->aa = AA_VALUE;
+    header->rd = RD_VALUE;
+    header->ra = RA_VALUE;
+    header->z = Z_VALUE;
+    return header;
+}
+dns_question *init_dns_question()
+{
+    dns_question *question = calloc(1, sizeof(dns_question));
+    return question;
+}
+dns_response *init_dns_response()
+{
+    dns_response *response = calloc(1, sizeof(dns_response));
+    return response;
+}
+
+dns_pkt *init_dns_pkt()
+{
+    dns_pkt *pkt = calloc(1, sizeof(dns_pkt));
+    return pkt;
+}
+
+void parse_header(dns_pkt *pkt, void *data)
+{
+    dns_header *header = init_dns_header();
+    memcpy(header, data, HEADER_LENGTH);
+    header->id = ntohs(header->id);
+    header->qdcount = ntohs(header->qdcount);
+    header->ancount = ntohs(header->ancount);
+    header->nscount = ntohs(header->nscount);
+    header->arcount = ntohs(header->arcount);
+    pkt->header = *header;
+}
+
+dns_question *parse_question(dns_pkt *pkt)
+{
+    dns_question *question = init_dns_question();
+    question = (dns_question *)pkt->data;
+    return question;
+}
+
+dns_pkt *parse_query(void *data, dns_question *question, size_t pkt_size)
+{
+    dns_pkt *pkt = init_dns_pkt();
+
+    parse_header(pkt, data);
+    memcpy(pkt->data, ((char *)data) + HEADER_LENGTH, pkt_size);
+    if (pkt->header.qdcount == 1)
+        parse_question(pkt);
+    return pkt;
+}
+
+
+
---
+++ parse_request/parse_request.h
@@ -0,0 +1,70 @@
+//
+// Created by denis on 26/09/2020.
+//
+
+#ifndef DNS_SERVER_PARSE_REQUEST_H
+#define DNS_SERVER_PARSE_REQUEST_H
+
+#include <sys/types.h>
+
+#define OPCODE_VALUE 0
+#define AA_VALUE 1
+#define RD_VALUE 0
+#define RA_VALUE 0
+#define Z_VALUE 0
+
+#define HEADER_LENGTH 12
+#define QUESTION_LENGTH 16
+#define AUTHORITY_LENGTH 16
+#define ADDITIONAL_LENGTH 16
+
+typedef struct{
+    u_int16_t id;
+    u_int16_t qr:1;
+    u_int16_t opcode:4;
+    u_int16_t aa:1;
+    u_int16_t tc:1;
+    u_int16_t rd:1;
+    u_int16_t ra:1;
+    u_int16_t z:3;
+    u_int16_t rcode:3;
+    u_int16_t qdcount; // questions
+    u_int16_t ancount; // response
+    u_int16_t nscount; // authority
+    u_int16_t arcount; // additional
+}dns_header;
+
+typedef struct{
+    char *qname;
+    u_int16_t qtype;
+    u_int16_t qclass;
+}dns_question;
+
+typedef struct{
+    char *name;
+    u_int16_t type;
+    u_int16_t class;
+    u_int32_t ttl;
+    u_int16_t  rdlength;
+    char *rdata;
+}dns_response, dns_authority, dns_additional;
+
+typedef struct{
+    dns_header header;
+    char *data;
+}dns_pkt;
+
+void print_header(dns_header *dns_header);
+void print_question(dns_question *dns_question);
+void print_response(dns_response *dns_response);
+
+void print_pkt(dns_pkt *pkt);
+
+dns_header *init_dns_header();
+dns_question *init_dns_question();
+dns_response *init_dns_response();
+
+dns_question *parse_question(dns_pkt *pkt);
+dns_pkt *parse_query(void *data, dns_question *question, size_t size);
+
+#endif //DNS_SERVER_PARSE_REQUEST_H
